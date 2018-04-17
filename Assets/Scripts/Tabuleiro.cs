4f87b12453a4c7c67431250a2006e7c738c1f26a
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2018-04-16 23:07:48 -0300

    adicionado inicialização de lista de posicoes e matriz de tabuleiro

--- Assets/Scripts/Tabuleiro.cs
+++ Assets/Scripts/Tabuleiro.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class Tabuleiro : MonoBehaviour {
 
-	string[][] matriz_tabuleiro = new string[8][8];
-	List<GameObject> pedras_blue = new List<GameObject>();
-	List<GameObject> pedras_red = new List<GameObject>();
+	public List<GameObject> pedrasBlue;
+	public List<GameObject> pedrasRed;
+	public GameObject posicoesAgrupamento;
 
+	private string[][] matrizTabuleiro = new string[8][];
+	private List<GameObject> posicoes;
 
 	// Use this for initialization
 	void Start () {
-			
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		initialize_matriz_tabuleiro();
+		posicoes = preenche_lista_posicoes ();
+		Debug.Log ("Qtd de posicções:" + posicoes.Count);
 	}
 
-	boolean makeMove(){
+	bool makeMove(){
 		//TODO Consulta máquina de regras para ver se jogada é válida
 		// return false se não for
 		//TODO callMakeValidMove()
+		return true;
 	}
 
 
-	private boolean makeValidMove(GameObject dama, int posx, int posy){
+	private bool makeValidMove(GameObject dama, int posx, int posy){
+		return true;
+	}
+
+	private void initialize_matriz_tabuleiro(){
+		int i = 0;
+		for (i = 0; i < matrizTabuleiro.Length; i++) 
+			matrizTabuleiro [i] = new string[8];
+	}
+
+	private List<GameObject> preenche_lista_posicoes(){
+		int qtd_posicoes = posicoesAgrupamento.transform.childCount, i = 0;
+		List<GameObject> posicoes_new = new List<GameObject> ();
+		for (i = 0; i < qtd_posicoes; i++)
+			posicoes_new.Add(posicoesAgrupamento.transform.GetChild(i).gameObject);
+
+		return posicoes_new;
+	}
 		
-	}
 }
