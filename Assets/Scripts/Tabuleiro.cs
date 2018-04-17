c00f34fde89816ca9bafeea6a80d0e9137263d65
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2018-04-16 21:36:52 -0300

    adicionando as damas ao projeto e as posicoes das dasmas assim como algume sboço do escript inicial

---
+++ Assets/Scripts/Tabuleiro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tabuleiro : MonoBehaviour {
+
+	string[][] matriz_tabuleiro = new string[8][8];
+	List<GameObject> pedras_blue = new List<GameObject>();
+	List<GameObject> pedras_red = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	boolean makeMove(){
+		//TODO Consulta máquina de regras para ver se jogada é válida
+		// return false se não for
+		//TODO callMakeValidMove()
+	}
+
+
+	private boolean makeValidMove(GameObject dama, int posx, int posy){
+		
+	}
+}
