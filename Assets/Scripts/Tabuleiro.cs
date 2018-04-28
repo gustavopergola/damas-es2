dab1daf7281303682a7907813ef9201da425cd39
João Pedro Sá Medeiros <jpsamedeiros@gmail.com>
2018-04-28 19:15:21 -0300

    #1 Criar matriz

--- Assets/Scripts/Tabuleiro.cs
+++ Assets/Scripts/Tabuleiro.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Tabuleiro : MonoBehaviour {
@@ -8,7 +9,7 @@ public class Tabuleiro : MonoBehaviour {
 	public List<GameObject> pedrasRed;
 	public GameObject posicoesAgrupamento;
 
-	private string[][] matrizTabuleiro = new string[8][];
+	private GameObject[,] matrizTabuleiro = new GameObject[8,8];
 	private List<GameObject> posicoes;
 
 	// Use this for initialization
@@ -31,15 +32,30 @@ public class Tabuleiro : MonoBehaviour {
 	}
 
 	private void initialize_matriz_tabuleiro(){
-		int i = 0;
-		for (i = 0; i < matrizTabuleiro.Length; i++) 
-			matrizTabuleiro [i] = new string[8];
-	}
+        int col, lin, contador = 0;
+        string resposta = "";
+        Debug.Log("QUE QUE ISSO? ", posicoesAgrupamento.transform);
+        Debug.Log(matrizTabuleiro.GetLength(0));
+        Debug.Log(matrizTabuleiro.GetLength(1));
+        for (col = matrizTabuleiro.GetLength(0)-1; col >= 0; col--)
+        { 
+            for(lin = matrizTabuleiro.GetLength(1)-1; lin >= 0; lin--)
+            {
+                matrizTabuleiro[col, lin] = posicoesAgrupamento.transform.GetChild(contador).gameObject;
+                resposta = resposta + matrizTabuleiro[col, lin];
+                contador++;
+                Debug.Log("COLUNA: " + col + " LINHA: " + lin + "     ELEMENTO: " + matrizTabuleiro[col, lin]);
+                //Debug.Log("COLUNA: "+col+" LINHA: "+lin+"CONTADOR: "+contador+"ELEMENTO: "+ posicoesAgrupamento.transform.GetChild(contador).gameObject);
+            }
+            Debug.Log(resposta);
+            resposta = "";
+        }
+    }
 
 	private List<GameObject> preenche_lista_posicoes(){
 		int qtd_posicoes = posicoesAgrupamento.transform.childCount, i = 0;
 		List<GameObject> posicoes_new = new List<GameObject> ();
-		for (i = 0; i < qtd_posicoes; i++)
+        for (i = 0; i < qtd_posicoes; i++)
 			posicoes_new.Add(posicoesAgrupamento.transform.GetChild(i).gameObject);
 
 		return posicoes_new;
