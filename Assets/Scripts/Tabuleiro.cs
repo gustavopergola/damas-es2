14321d1c0ac6cf766ccd82c6dcf23edf60d650a7
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-04-29 13:30:50 -0300

    #1 Remoção de logs -Remoção de Logs -Comentado o código antigo das posições

--- Assets/Scripts/Tabuleiro.cs
+++ Assets/Scripts/Tabuleiro.cs
@@ -15,8 +15,6 @@ public class Tabuleiro : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		initialize_matriz_tabuleiro();
-		posicoes = preenche_lista_posicoes ();
-		Debug.Log ("Qtd de posicções:" + posicoes.Count);
 	}
 
 	bool makeMove(){
@@ -33,32 +31,23 @@ public class Tabuleiro : MonoBehaviour {
 
 	private void initialize_matriz_tabuleiro(){
         int col, lin, contador = 0;
-        string resposta = "";
-        Debug.Log("QUE QUE ISSO? ", posicoesAgrupamento.transform);
-        Debug.Log(matrizTabuleiro.GetLength(0));
-        Debug.Log(matrizTabuleiro.GetLength(1));
         for (col = matrizTabuleiro.GetLength(0)-1; col >= 0; col--)
         { 
             for(lin = matrizTabuleiro.GetLength(1)-1; lin >= 0; lin--)
             {
                 matrizTabuleiro[col, lin] = posicoesAgrupamento.transform.GetChild(contador).gameObject;
-                resposta = resposta + matrizTabuleiro[col, lin];
                 contador++;
-                Debug.Log("COLUNA: " + col + " LINHA: " + lin + "     ELEMENTO: " + matrizTabuleiro[col, lin]);
-                //Debug.Log("COLUNA: "+col+" LINHA: "+lin+"CONTADOR: "+contador+"ELEMENTO: "+ posicoesAgrupamento.transform.GetChild(contador).gameObject);
             }
-            Debug.Log(resposta);
-            resposta = "";
         }
     }
 
-	private List<GameObject> preenche_lista_posicoes(){
+	/*private List<GameObject> preenche_lista_posicoes(){
 		int qtd_posicoes = posicoesAgrupamento.transform.childCount, i = 0;
 		List<GameObject> posicoes_new = new List<GameObject> ();
         for (i = 0; i < qtd_posicoes; i++)
 			posicoes_new.Add(posicoesAgrupamento.transform.GetChild(i).gameObject);
 
 		return posicoes_new;
-	}
+	}*/
 		
 }
