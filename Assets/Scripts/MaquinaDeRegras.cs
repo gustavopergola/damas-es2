c9387705cd770d0ecb3870deacfbd7a9122a2773
gabrielcs23 <gabrielcs23@hotmail.com>
2018-05-03 11:59:53 -0300

    highlight verificando comer com pedras

--- Assets/Scripts/MaquinaDeRegras.cs
+++ Assets/Scripts/MaquinaDeRegras.cs
@@ -36,28 +36,9 @@ public class MaquinaDeRegras : MonoBehaviour
         }
         if (tabuleiro[x, y] == dama)
             return HighlightDamas(tabuleiro, x, y, pecaInimiga, damaInimiga);
-        bool comer = false;
         List<List<int>> posicoes = new List<List<int>>();
-        if (x + 2 < 8 && y + 2 < 8 && (tabuleiro[x + 1, y + 1] == pecaInimiga || tabuleiro[x + 1, y + 1] == damaInimiga))
-        {
-            posicoes.Add(new List<int> { x + 2, y + 2 });
-            comer = true;
-        }
-        if (x - 2 >= 0 && y + 2 < 8 && (tabuleiro[x - 1, y + 1] == pecaInimiga || tabuleiro[x + 1, y + 1] == damaInimiga))
-        {
-            posicoes.Add(new List<int> { x - 2, y + 2 });
-            comer = true;
-        }
-        if (x + 2 < 8 && y - 2 >= 0 && (tabuleiro[x + 1, y - 1] == pecaInimiga || tabuleiro[x + 1, y - 1] == damaInimiga)){
-            posicoes.Add(new List<int> { x + 2, y - 2 });
-            comer = true;
-        }
-        if (x - 2 < 8 && y - 2 >= 0 && (tabuleiro[x - 1, y - 1] == pecaInimiga || tabuleiro[x - 1, y - 1] == damaInimiga))
-        {
-            posicoes.Add(new List<int> { x - 2, y - 2 });
-            comer = true;
-        }
-        if (comer == true)
+        posicoes = LeiDaMaioria(tabuleiro, x, y, pecaInimiga, damaInimiga);
+        if (posicoes.Count > 0)
             return posicoes;
         if (x + 1 < 8 && y + 1 < 8 && tabuleiro[x + 1, y + 1] == 0)
             posicoes.Add(new List<int> { x + 1, y + 1 });
@@ -73,22 +54,22 @@ public class MaquinaDeRegras : MonoBehaviour
         bool[] controle = {false, true};
         for(i = x, j = y; i < 8 && j < 8; i++, j++){
             controle = checkFor(posicoes, tabuleiro, i, j, pecaInimiga, damaInimiga, controle[0]);
-            if(!controle[1]) 
+            if(!controle[1]) // não há movimentação nessa diagonal
                 break;
         }
         for(i = x, j = y; i < 8 && j >= 0; i++, j--){
             controle = checkFor(posicoes, tabuleiro, i, j, pecaInimiga, damaInimiga, controle[0]);
-            if(!controle[1]) 
+            if(!controle[1]) // não há movimentação nessa diagonal
                 break;
         }
         for(i = x, j = y; i >= 0 && j < 8; i--, j++){
             controle = checkFor(posicoes, tabuleiro, i, j, pecaInimiga, damaInimiga, controle[0]);
-            if(!controle[1]) 
+            if(!controle[1]) // não há movimentação nessa diagonal
                 break;
         }
         for(i = x, j = y; i >= 0 && j >= 0; i--, j--){
             controle = checkFor(posicoes, tabuleiro, i, j, pecaInimiga, damaInimiga, controle[0]);
-            if(!controle[1]) 
+            if(!controle[1]) // não há movimentação nessa diagonal
                 break;
         }
         return posicoes;
@@ -96,19 +77,82 @@ public class MaquinaDeRegras : MonoBehaviour
 
     private bool[] checkFor(List<List<int>> posicoes, int[,] tabuleiro, int i, int j, int pecaInimiga, int damaInimiga, bool comer){
         bool[] retorno = {comer, true};
-        if (!retorno[0] && tabuleiro[i, j] == 0){
+        if (!comer && tabuleiro[i, j] == 0){
             posicoes.Add(new List<int> { i, j });
             return retorno;
         }
         else if(tabuleiro[i, j] == damaInimiga || tabuleiro[i, j] == pecaInimiga){
-            retorno[0] = true;
+            retorno[0] = true; // comer = true
+            // chamar LeiDaMaioria aqui??
             posicoes.Add(new List<int> { i, j });
             return retorno;
         }
         else{
-            retorno[1] = false;
+            retorno[1] = false; // não há movimentação
             return retorno;
         }
+    }
+
+    private List<List<int>> LeiDaMaioria(int [,] tabuleiro, int x, int y, int pecaInimiga, int damaInimiga) {
+        List<List<int>> pos1 = new List<List<int>>();
+        List<List<int>> pos2 = new List<List<int>>();
+        List<List<int>> pos3 = new List<List<int>>();
+        List<List<int>> pos4 = new List<List<int>>();
+        List<List<int>> posAux = new List<List<int>>();
+        if (x + 2 < 8 && y + 2 < 8 && (tabuleiro[x + 1, y + 1] == pecaInimiga || tabuleiro[x + 1, y + 1] == damaInimiga))
+        {
+            pos1.Add(new List<int> { x + 2, y + 2 });
+            posAux = LeiDaMaioria(tabuleiro, x+2, y+2, pecaInimiga, damaInimiga);
+            if(posAux.Count != 0){
+                foreach (List<int> pos in posAux)
+                {
+                    pos1.Add(pos);
+                }
+            }
+        }
+        if (x - 2 >= 0 && y + 2 < 8 && (tabuleiro[x - 1, y + 1] == pecaInimiga || tabuleiro[x + 1, y + 1] == damaInimiga))
+        {
+            pos2.Add(new List<int> { x - 2, y + 2 });
+            posAux = LeiDaMaioria(tabuleiro, x-2, y+2, pecaInimiga, damaInimiga);
+            if(posAux.Count != 0){
+                foreach (List<int> pos in posAux)
+                {
+                    pos2.Add(pos);
+                }
+            }
+        }
+        if (x + 2 < 8 && y - 2 >= 0 && (tabuleiro[x + 1, y - 1] == pecaInimiga || tabuleiro[x + 1, y - 1] == damaInimiga)){
+            pos3.Add(new List<int> { x + 2, y - 2 });
+            posAux = LeiDaMaioria(tabuleiro, x+2, y-2, pecaInimiga, damaInimiga);
+            if(posAux.Count != 0){
+                foreach (List<int> pos in posAux)
+                {
+                    pos3.Add(pos);
+                }
+            }
+        }
+        if (x - 2 < 8 && y - 2 >= 0 && (tabuleiro[x - 1, y - 1] == pecaInimiga || tabuleiro[x - 1, y - 1] == damaInimiga))
+        {
+            pos4.Add(new List<int> { x - 2, y - 2 });
+            posAux = LeiDaMaioria(tabuleiro, x-2, y-2, pecaInimiga, damaInimiga);
+            if(posAux.Count != 0){
+                foreach (List<int> pos in posAux)
+                {
+                    pos4.Add(pos);
+                }
+            }
+        }
+        return max(pos1, pos2, pos3, pos4);
+    }
+
+    private List<List<int>> max(List<List<int>> lista1, List<List<int>> lista2, List<List<int>> lista3, List<List<int>> lista4) {
+        if(lista1.Count >= lista2.Count && lista1.Count >= lista3.Count && lista1.Count >= lista4.Count)
+            return lista1;
+        if(lista2.Count >= lista1.Count && lista2.Count >= lista3.Count && lista2.Count >= lista4.Count)
+            return lista2;
+        if(lista3.Count >= lista1.Count && lista3.Count >= lista2.Count && lista3.Count >= lista4.Count)
+            return lista3;
+        else return lista4;
     }
 }
 
