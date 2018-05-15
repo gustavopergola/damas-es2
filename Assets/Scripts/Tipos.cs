86a98de7ba4e4371138c636d887d88b5627b05b0
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-05-15 00:09:19 -0300

    Criar classe tipos

---
+++ Assets/Scripts/Tipos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+namespace TiposNS
+{
+	public class Tipos : MonoBehaviour {
+		public const int pecaComum = 1;
+		public const int dama = 2;
+		private const int mod = 2;
+
+		public static int getPecaJogador1(){
+			return Tipos.pecaComum;
+		}
+		public static int getDamaJogador1(){
+			return Tipos.dama;
+		}
+		public static int getPecaJogador2(){
+			return Tipos.pecaComum + mod;
+		}
+		public static int getDamaJogador2(){
+			return Tipos.dama + mod;
+		}
+		public static bool isJogador1(int peca){
+			return peca == Tipos.getPecaJogador1() || peca == Tipos.getDamaJogador1();
+		}
+		public static bool isJogador2(int peca){
+			return peca == Tipos.getPecaJogador2() || peca == Tipos.getDamaJogador2();
+		}
+		public static bool isPecaComum(int peca){
+			return peca == Tipos.getPecaJogador1() || peca == Tipos.getPecaJogador2();
+		}
+		public static bool isDama(int peca){
+			return peca == Tipos.getDamaJogador1() || peca == Tipos.getDamaJogador2();
+		}
+	}
+}
