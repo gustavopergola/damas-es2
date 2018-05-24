7a319848ce163cb7cab9f70f9c0c202f632b524c
gabrielcs23 <gabrielcs23@hotmail.com>
2018-05-24 14:13:25 -0300

    classe Jogada: construtor e método Comeu

--- Assets/Scripts/Jogada.cs
+++ Assets/Scripts/Jogada.cs
@@ -5,4 +5,17 @@ public class Jogada
     public List<int[]> movimentos;
     public List<int[]> pecasComidas;
     bool virouDama;
+
+    public Jogada()
+    {
+        this.movimentos = new List<int[]>();
+        this.virouDama = false;
+    }
+
+    public void Comeu(int x, int y)
+    {
+        if (this.pecasComidas == null)
+            this.pecasComidas = new List<int[]>();
+        this.pecasComidas.Add(new int[2] {x,y});
+    }
 }
