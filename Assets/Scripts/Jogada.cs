2cc0868f5ee6266b533f9543a2788111ad249fa3
gabrielcs23 <gabrielcs23@hotmail.com>
2018-05-26 00:08:08 -0300

    simplificação da Jogada

--- Assets/Scripts/Jogada.cs
+++ Assets/Scripts/Jogada.cs
@@ -9,13 +9,7 @@ public class Jogada
     public Jogada()
     {
         this.movimentos = new List<int[]>();
+        this.pecasComidas = new List<int[]>();
         this.virouDama = false;
-    }
-
-    public void Comeu(int x, int y)
-    {
-        if (this.pecasComidas == null)
-            this.pecasComidas = new List<int[]>();
-        this.pecasComidas.Add(new int[2] {x,y});
     }
 }
