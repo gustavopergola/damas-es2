96f6c46d17a467f9a249078d88cc4aa9cbdba9f6
andrelpoj <andrelpoj@gmail.com>
2018-05-28 20:03:05 -0300

    #7 Estado utilizando jogada

--- Assets/Scripts/Estado.cs
+++ Assets/Scripts/Estado.cs
@@ -5,19 +5,17 @@ using TiposNS;
 
 public class Estado : MonoBehaviour {
 
-	public int[,] board;
-	public int currentPlayer;
-	public int[] lastMove;
-	public int playsCount;
-	public int playsWithoutCapture;
+    public int[,] tabuleiro;
+    public int jogadorAtual;
+	public Jogada ultimaJogada;
+    //public int jogadasCount;
+    //public int movimentosSemCaptura;
 	public bool gameOver = false;
 
-	public Estado(int[,] board, int currentPlayer, int[] lastMove, int playsCount, int playsWithoutCapture = 0){
-		this.board = (int[,])board.Clone();
-		this.currentPlayer = currentPlayer;
-		this.lastMove = lastMove;
-		this.playsCount = playsCount;
-		this.playsWithoutCapture = playsWithoutCapture;
+	public Estado(int[,] tabuleiro, int jogadorAtual, Jogada ultimaJogada){
+        this.tabuleiro = (int[,])tabuleiro.Clone();
+        this.jogadorAtual = jogadorAtual;
+        this.ultimaJogada = ultimaJogada;
 	}
 
 	public void print(){
@@ -38,58 +36,65 @@ public class Estado : MonoBehaviour {
 		}
 	}
 
-	private void registerGameIsOver(){
-		int num_player1_pieces = 0;
-		int num_player2_pieces = 0;
+	//private void registerGameIsOver(){
+	//	int num_player1_pieces = 0;
+	//	int num_player2_pieces = 0;
+    //
+	//	foreach (int piece in this.board){
+	//		if (Tipos.isJogador1(piece)) num_player1_pieces++;
+	//		if (Tipos.isJogador2(piece)) num_player2_pieces++;
+	//		//if((num_player1_pieces!=0) && (num_player2_pieces!=0)) break;	
+	//	}
+	//	if((num_player1_pieces==0) || (num_player2_pieces==0)){
+	//		this.gameOver = true;
+	//	}
+	//	else{
+	//		this.gameOver = false || this.checkDraw();
+	//	}
+	//}
 
-		foreach (int piece in this.board){
-			if (Tipos.isJogador1(piece)) num_player1_pieces++;
-			if (Tipos.isJogador2(piece)) num_player2_pieces++;
+    //public bool checkDraw()
+    //{
+    //    return this. >= 20;
+    //}
 
-			//if((num_player1_pieces!=0) && (num_player2_pieces!=0)) break;	
-		}
-
-		if((num_player1_pieces==0) || (num_player2_pieces==0)){
-			this.gameOver = true;
-		}
-		else{
-			this.gameOver = false || this.checkDraw();
-		}
-
-	}
-
-	public bool gameIsOver(){
+    public bool gameIsOver(){
 		return this.gameOver;
 	}
 
-	public bool checkDraw(){
-		return this.playsWithoutCapture >= 20;
-	}
-
 	public int getOpponent(){            
-		if (this.currentPlayer == 1) return 2;
+		if (this.jogadorAtual== 1) return 2;
 		else return 1;
 	}
 
-	public static Estado result(Estado oldState, int[] action){
-		Estado newState = new Estado(oldState.board, oldState.currentPlayer, oldState.lastMove, oldState.playsCount, oldState.playsWithoutCapture);
-		
-		//TODO Concertar quando tiver a função na máquina de regras
-		/*if(RuleMachine.isAttackMove(action, newState.board)){
+	public static Estado result(Estado antigo, Jogada acao){
+		Estado novo = new Estado(antigo.tabuleiro, antigo.jogadorAtual, antigo.ultimaJogada);
+
+        //TODO Concertar quando tiver a função na máquina de regras
+        /*if(RuleMachine.isAttackMove(action, newState.board)){
 			newState.playsWithoutCapture = 0;
 		}else {
 			newState.playsWithoutCapture++;
 		}*/
 
-		int piece = newState.board[action[0], action[1]];
+        novo.jogadorAtual = novo.jogadorAtual == 1 ? 2 : 1;
+        novo.ultimaJogada = acao;
+        //novo.jogadasCount++;
+        //novo.registerGameIsOver();
+
+        
+        tabuleiro[acao.movimentos[0], acao.movimentos[0]] = ;
+
+        foreach(var peca in acao.pecasComidas){
+            novo.tabuleiro[peca[0], peca[1]] = 0;
+        }
+        
+
+        int piece = newState.board[action[0], action[1]];
 		newState.board[action[0], action[1]] = Tipos.vazio;
 		newState.board[action[2], action[3]] = piece;
-		newState.currentPlayer = newState.currentPlayer == 1 ? 2 : 1;
-		newState.lastMove = action;
-		newState.playsCount++;
 		
-		newState.registerGameIsOver();
-
+		
 		return newState;
 	}
 
