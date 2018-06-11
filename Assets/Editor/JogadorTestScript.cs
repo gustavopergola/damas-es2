8a65881078df9150067e28b6f6c924254f7f0974
VictoralcNunes <victoralcnunes@hotmail.com>
2018-06-11 15:53:14 -0300

    #32 Iniciando testes

---
+++ Assets/Editor/JogadorTestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Scripts.TiposNS;
+
+public class JogadorTestScript {
+
+    [Test]
+    public void NewTestScriptSimplePasses222222222222222() {
+        // Use the Assert class to test conditions.
+        var peca1 = getPecaJogador1();
+
+        Assert.AreEquals(1, peca1);
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
---
+++ Assets/Editor/TiposTestScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TiposTestScript : MonoBehaviour{
+
+    [Test]
+    public void DeveRetornarAPecaDoJogador1() {
+        GameObject pecaSpt;
+
+        pecaSpt.GetComponent<Tipos>();
+        // Use the Assert class to test conditions.
+        var peca1 = pecaSpt.getPecaJogador1();
+
+        Assert.Equals(1, peca1);
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
